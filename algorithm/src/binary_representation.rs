927f439e979762ebfb70f1531c27bb20cbc9a092
Tranduy1dol <tmd.id2004@gmail.com>
2025-03-01 16:18:18 +0000

    feat: binary representation

---
+++ algorithm/src/binary_representation.rs
@@ -0,0 +1,25 @@
+pub fn binary_representation(mut input: usize) -> anyhow::Result<String> {
+    let mut result: String = "".to_string();
+    while !input.eq(&0) {
+        let char = input % 2;
+        result.push(char.to_string().parse()?);
+        input /= 2;
+    }
+    Ok(result)
+}
+
+mod test {
+    use crate::binary_representation::binary_representation;
+
+    #[test]
+    fn test_binary_representation() {
+        let result = binary_representation(2).unwrap();
+        assert_eq!(result, "01".to_string());
+    }
+
+    #[test]
+    fn test_binary_representation2() {
+        let result = binary_representation(3).unwrap();
+        assert_eq!(result, "11".to_string());
+    }
+}
