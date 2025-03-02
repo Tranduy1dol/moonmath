0d7db524ca08fc2a4e0200f4a230ac83392636e2
Tranduy1dol <tmd.id2004@gmail.com>
2025-03-02 09:16:20 +0000

    feat: poly long division

--- algorithm/src/long_division.rs
+++ algorithm/src/long_division.rs
@@ -23,25 +23,48 @@ pub fn long_division(dividend: i64, divisor: i64) -> anyhow::Result<(i64, u64)> 
     Ok((quotient, carry as u64))
 }
 
-// pub fn poly_long_division(a: Vec<u64>, b: Vec<u64>) -> anyhow::Result<(Vec<i64>, Vec<i64>)> {
-//     let lc_b = b.last().unwrap();
-//     let d = b.len() as u64;
-//     let q: Vec<u64> = vec![];
-//     let p = a.clone();
-//
-//     while p.len() as u64 > d {
-//
-//     }
-//
-//     Ok()
-// }
+pub fn poly_long_division(a: Vec<i64>, b: Vec<i64>) -> anyhow::Result<(Vec<i64>, Vec<i64>)> {
+    let mut p = a.clone();
+    let mut q = vec![0i64; a.len() - b.len() + 1];
+
+    let d = b.len() as u64 - 1;
+
+    while p.len() as u64 - 1 >= d {
+        let s = p.last().unwrap() / b.last().unwrap();
+        q[p.len() - b.len()] = s;
+
+        let mut s_product_b = vec![0i64; p.len()];
+        let degree_s_product_b = s_product_b.len();
+        for i in 0..b.len() {
+            s_product_b[degree_s_product_b - b.len() + i] = s * b[i];
+        }
+
+        for i in 0..p.len() {
+            p[i] -= s_product_b[i];
+        }
+
+        if *p.last().unwrap() == 0 {
+            p.pop();
+        }
+    }
+
+    Ok((q, p))
+}
 
 mod test {
-    use crate::long_division::long_division;
+    use crate::long_division::{long_division, poly_long_division};
 
     #[test]
     fn test_long_division() {
         let result = long_division(45678, 90).unwrap();
         assert_eq!(result, (507, 48));
+    }
+
+    #[test]
+    fn test_poly_long_division() {
+        let a = vec![-9, 0, 0, 2, 0, 1];
+        let b = vec![-1, 4, 1];
+        let result = poly_long_division(a, b).unwrap();
+        assert_eq!(result, (vec![-80, 19, -4, 1], vec![-89, 339]));
     }
 }
